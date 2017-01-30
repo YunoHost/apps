b52094a01f042db83b19b8e28b3f76bff2d7e6a3
Laurent Peuch <cortex@worlddomination.be>
2017-01-30 23:47:59 +0100

    [enh] change_level.py uses int not and have sanity checks for that

--- change_level.py
+++ change_level.py
@@ -22,6 +22,14 @@ if __name__ == '__main__':
         print "Error: app '%s' is not present in %s" % (app_id, app_list_name)
         sys.exit(1)
 
-    app_list[app_id]["level"] = level
+    if not level.isdigit():
+        print "Error: app level must be a number, it's '%s'" % level
+        sys.exit(1)
+
+    if not 0 <= int(level) <= 10:
+        print "Error: app level must be between 0 and 10, it's '%s'" % level
+        sys.exit(1)
+
+    app_list[app_id]["level"] = int(level)
 
     open(app_list_name, "w").write("\n".join(json.dumps(app_list, indent=4, sort_keys=True).split(" \n")) + "\n")
