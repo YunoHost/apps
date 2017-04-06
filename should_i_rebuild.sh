9a86ea03ed71c93bf4622405c633105647230614
Laurent Peuch <cortex@worlddomination.be>
2017-04-06 13:43:26 +0200

    [mod] always run list_builder if there is a new commit\n\nSince now we also need to run list_builder when the translations have been updated, testing exactly if he needs to run is more complicated, but the cache system has been improved so we can run it for every new commits \o/

--- should_i_rebuild.sh
+++ should_i_rebuild.sh
@@ -14,31 +14,19 @@ then
     exit 1
 fi
 
-before_official=$(sha256sum official.json)
-before_community=$(sha256sum community.json)
-before_dev=$(sha256sum dev.json)
 before_pull_commit=$(git show HEAD | head -n 1)
 
 git pull
 
-if [ "$before_official" != "$(sha256sum official.json)" ]
-then
-    python ./list_builder.py -g $1 official.json
-fi
-
-if [ "$before_community" != "$(sha256sum community.json)" ]
-then
-    python ./list_builder.py -g $1 community.json
-fi
-
-if [ "$before_dev" != "$(sha256sum dev.json)" ]
-then
-    python ./list_builder.py -g $1 dev.json
-fi
 
 if [ "$before_pull_commit" != "$(git show HEAD | head -n 1)" ]
 then
+    python ./list_builder.py -g $1 official.json
+    python ./list_builder.py -g $1 community.json
+    python ./list_builder.py -g $1 dev.json
+
     python ./update_translations.py official-build.json community-build.json dev-build.json
+
     if [ "$(git status -s| grep 'M locales/en.json')" ]
     then
         git add locales/en.json
