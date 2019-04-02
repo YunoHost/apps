dadc69d94784caf575e090556360bb367c8b8bfc
Alexandre Aubin <alexandre.aubin@cern.ch>
2019-04-03 01:20:27 +0200

    Uuuh re-add high-quality + working state, c.f. commit from Maniack

--- merge_official_and_community.py
+++ merge_official_and_community.py
@@ -3,6 +3,11 @@ import json
 community = json.loads(open("community.json").read())
 official = json.loads(open("official.json").read())
 
+# Add high quality and set working state for official apps
+for app, infos in official.items():
+    infos["high_quality"] = True
+    infos["state"] = "working"
+
 merged = community
 merged.update(official)
 
