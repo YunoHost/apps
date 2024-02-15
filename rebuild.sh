2a5ec791514c22ba65e07e7f8b17e47232eed47b
Alexandre Aubin <alex.aubin@mailoo.org>
2024-02-15 17:15:08 +0100

    Add cron stuff

--- rebuild.sh
+++ rebuild.sh
@@ -6,5 +6,6 @@ log=$workdir/app_list_auto_update.log
 cd $workdir
 date >> $log
 git pull &>/dev/null
+cat cron | sed "s@__BASEDIR__@$workdir@g" > /etc/cron.d/app_list
 
 ./tools/list_builder.py &>> $log || sendxmpppy "[listbuilder] Rebuilding the application list failed miserably"
---
+++ sourcesautoupdate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+workdir=$(dirname "$0")
+log=$workdir/app_sources_auto_update.log
+
+cd $workdir
+date >> $log
+git pull &>/dev/null
+cat cron | sed "s@__BASEDIR__@$workdir@g" > /etc/cron.d/app_list
+
+python3 tools/autoupdate_app_sources/autoupdate_app_sources.py --commit-and-create-PR &> $log || sendxmpppy "[appsourcesautoupdate] App sources auto-update failed miserably"
