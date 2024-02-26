dacb57fb0a1e3c1413d3193bf54216498b42637c
Félix Piédallu <felix@piedallu.me>
2024-02-26 11:39:08 +0100

    Split apps_cache call, run apps_cache update when updating sources, and run -jfaster

--- rebuild.sh
+++ rebuild.sh
@@ -8,4 +8,5 @@ date >> $log
 git pull &>/dev/null
 cat cron | sed "s@__BASEDIR__@$workdir@g" > /etc/cron.d/app_list
 
-./tools/list_builder.py &>> $log || sendxmpppy "[listbuilder] Rebuilding the application list failed miserably"
+./tools/app_caches.py -j40 &>> $log || sendxmpppy "[listbuilder] Downloading the apps caches failed miserably"
+./tools/list_builder.py -j10 --no-update-cache &>> $log || sendxmpppy "[listbuilder] Rebuilding the application list failed miserably"
--- sourcesautoupdate.sh
+++ sourcesautoupdate.sh
@@ -8,6 +8,8 @@ date >> $log
 git pull &>/dev/null
 cat cron | sed "s@__BASEDIR__@$workdir@g" > /etc/cron.d/app_list
 
+./tools/app_caches.py -j40 &>> $log || sendxmpppy "[appsourcesautoupdate] Downloading the apps caches failed miserably"
+
 python3 tools/autoupdate_app_sources/autoupdate_app_sources.py \
     --edit --commit --pr --paste -j1 \
 &> $log || sendxmpppy "[appsourcesautoupdate] App sources auto-update failed miserably"
