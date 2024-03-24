02c021b9388b4bfabc8c2b5ddce695f1678cacfb
OniriCorpe <oniricorpe@yunohost.org>
2024-03-24 05:45:21 +0100

    chores: update & fix readme templates (#2164)

--- tools/readme_generator/tests/test_make_readme.py
+++ tools/readme_generator/tests/test_make_readme.py
@@ -1,46 +1,31 @@
 #!/usr/bin/env python3
 
-import difflib
 import tempfile
 import subprocess
 
 from pathlib import Path
 
-CWD = Path(__file__).resolve().parent
+TEST_DIRECTORY = Path(__file__).resolve().parent
 
 TEST_APP_NAME = "gotosocial_ynh"
 TEST_APP_REPO = "https://github.com/yunohost-apps/gotosocial_ynh"
 TEST_APP_COMMIT_ID = "8f788213b363a46a5b6faa8f844d86d4adac9446"
 
 
-def diff_files(file_a: Path, file_b: Path) -> bool:
-    lines_a = file_a.open(encoding="utf-8").readlines()
-    lines_b = file_b.open(encoding="utf-8").readlines()
-
-    diffs = list(
-        difflib.unified_diff(
-            lines_a, lines_b, fromfile="README.before.md", tofile="README.after.md"
-        )
-    )
-    print("".join(diffs))
-    return len(diffs) == 0
-
-
 def test_running_make_readme():
     with tempfile.TemporaryDirectory() as tempdir:
         tempdir = Path(tempdir)
-        DIRECTORY = tempdir / TEST_APP_NAME
+        temporary_tested_app_directory = tempdir / TEST_APP_NAME
 
-        subprocess.check_call(["git", "clone", "-q", TEST_APP_REPO, DIRECTORY])
+        subprocess.check_call(["git", "clone", "-q", TEST_APP_REPO, temporary_tested_app_directory])
         subprocess.check_call(
-            ["git", "checkout", "-q", TEST_APP_COMMIT_ID], cwd=DIRECTORY
+            ["git", "checkout", "-q", TEST_APP_COMMIT_ID], cwd=temporary_tested_app_directory
         )
 
         # Now run test...
-        subprocess.check_call([CWD.parent / "make_readme.py", DIRECTORY])
+        subprocess.check_call([TEST_DIRECTORY.parent / "make_readme.py", temporary_tested_app_directory])
 
-        assert diff_files(CWD / "README.md", DIRECTORY / "README.md")
-        assert diff_files(CWD / "README_fr.md", DIRECTORY / "README_fr.md")
+        assert open(TEST_DIRECTORY / "README.md").read() == open(temporary_tested_app_directory / "README.md").read()
 
 
 if __name__ == "__main__":
