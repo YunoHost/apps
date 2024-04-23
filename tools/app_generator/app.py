1599c7706235e41a025abb35dfaa5433fb3c7a00
Alexandre Aubin <4533074+alexAubin@users.noreply.github.com>
2024-04-23 20:11:15 +0200

    appgenerator: misc fixes (#2254)
    * appgenerator: docs -> doc
    
    * appgenerator: remove stuff related to DISCLAIMER.md which is not used in packaging v2 anymore

--- tools/app_generator/app.py
+++ tools/app_generator/app.py
@@ -539,16 +539,6 @@ Do not give the software name at the beginning, as it will be integrated an 'Ove
             "spellcheck": "false",
         },
     )
-    disclaimer = TextAreaField(
-        lazy_gettext(
-            "Type the DISCLAIMER.md file content, which list warnings and attention points."
-        ),
-        validators=[Optional()],
-        render_kw={
-            "class": "form-control",
-            "spellcheck": "false",
-        },
-    )
     pre_install = TextAreaField(
         lazy_gettext("Type the PRE_INSTALL.md file content"),
         validators=[Optional()],
@@ -751,25 +741,22 @@ def main_form_route():
         # app_files.append(AppFile("php", "conf/extra_php-fpm.conf"))
 
         if main_form.description.data:
-            app_files.append(AppFile("DESCRIPTION", "docs/DESCRIPTION.md"))
-
-        if main_form.disclaimer.data:
-            app_files.append(AppFile("DISCLAIMER", "docs/DISCLAIMER.md"))
+            app_files.append(AppFile("DESCRIPTION", "doc/DESCRIPTION.md"))
 
         if main_form.pre_install.data:
-            app_files.append(AppFile("PRE_INSTALL", "docs/PRE_INSTALL.md"))
+            app_files.append(AppFile("PRE_INSTALL", "doc/PRE_INSTALL.md"))
 
         if main_form.post_install.data:
-            app_files.append(AppFile("POST_INSTALL", "docs/POST_INSTALL.md"))
+            app_files.append(AppFile("POST_INSTALL", "doc/POST_INSTALL.md"))
 
         if main_form.pre_upgrade.data:
-            app_files.append(AppFile("PRE_UPGRADE", "docs/PRE_UPGRADE.md"))
+            app_files.append(AppFile("PRE_UPGRADE", "doc/PRE_UPGRADE.md"))
 
         if main_form.post_upgrade.data:
-            app_files.append(AppFile("POST_UPGRADE", "docs/POST_UPGRADE.md"))
+            app_files.append(AppFile("POST_UPGRADE", "doc/POST_UPGRADE.md"))
 
         if main_form.admin.data:
-            app_files.append(AppFile("ADMIN", "docs/ADMIN.md"))
+            app_files.append(AppFile("ADMIN", "doc/ADMIN.md"))
 
         template_dir = os.path.dirname(__file__) + "/templates/"
         for app_file in app_files:
