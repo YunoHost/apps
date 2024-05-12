cd59444f25cd5bfc36790ba00d346dfd5a1ef7c8
Alexandre Aubin <4533074+alexAubin@users.noreply.github.com>
2024-05-12 17:43:07 +0200

    Replace rebuild.sh / sourcesautoupdate.sh with a maintenance.sh to properly update cron job and auto-restart services when updating their code (#2323)
    * Replace rebuild.sh / sourcesautoupdate.sh with a maintenance.sh to properly update cron job and auto-restart services when updating their code
    
    * Mistakenly change the cron job periods
    
    * Apply suggestions from code review @_@

---
+++ maintenance.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+workdir=$(realpath $(dirname "$0"))
+cd $workdir
+
+function git_pull_and_update_cron_and_restart_services_if_needed()
+{
+    git pull &>/dev/null
+
+    # Cron
+    cat cron | sed "s@__BASEDIR__@$workdir@g" > /etc/cron.d/app_list
+
+    # App store
+    chown -R appstore store
+    pushd store >/dev/null
+    modified_after_service_start="$(find *.py translations/ templates/ assets/ -newermt "$(systemctl show --property=ActiveEnterTimestamp appstore | cut -d= -f2 | cut -d' ' -f2-3)-1")"
+    if [ -n "$modified_after_service_start" ]
+    then
+        pushd assets >/dev/null
+            ./tailwindcss-linux-x64 --input tailwind-local.css --output tailwind.css --minify
+        popd >/dev/null
+        systemctl restart appstore
+        sleep 3
+    fi
+    popd >/dev/null
+
+    systemctl --quiet is-active appstore || sendxmpppy "[appstore] Uhoh, failed to (re)start the appstore service?"
+
+    # App generator
+    chown -R appgenerator tools/app_generator
+    pushd tools/app_generator >/dev/null
+    modified_after_service_start="$(find *.py translations/ templates/ static/ -newermt "$(systemctl show --property=ActiveEnterTimestamp appgenerator | cut -d= -f2 | cut -d' ' -f2-3)-1")"
+    if [ -n "$modified_after_service_start" ]
+    then
+        pushd assets >/dev/null
+            ./tailwindcss-linux-x64 --input tailwind-local.css --output tailwind.css --minify
+        popd >/dev/null
+        systemctl restart appgenerator
+        sleep 3
+    fi
+    popd >/dev/null
+
+    systemctl --quiet is-active appgenerator || sendxmpppy "[appgenerator] Uhoh, failed to (re)start the appgenerator service?"
+
+    # Autoreadme
+    pushd tools/readme_generator >/dev/null
+    modified_after_service_start="$(find *.py translations/ templates/ -newermt "$(systemctl show --property=ActiveEnterTimestamp autoreadme | cut -d= -f2 | cut -d' ' -f2-3)-1")"
+    if [ -n "$modified_after_service_start" ]
+    then
+        systemctl restart autoreadme
+        sleep 3
+    fi
+    popd >/dev/null
+
+    systemctl --quiet is-active autoreadme || sendxmpppy "[autoreadme] Uhoh, failed to (re)start the autoreadme service?"
+}
+
+function rebuild_catalog()
+{
+    log=$workdir/app_list_auto_update.log
+    date >> $log
+    git_pull_and_update_cron_and_restart_services_if_needed
+    ./tools/list_builder.py &>> $log || sendxmpppy "[listbuilder] Rebuilding the application list failed miserably"
+}
+
+function autoupdate_app_sources()
+{
+    log=$workdir/app_sources_auto_update.log
+    date >> $log
+    git_pull_and_update_cron_and_restart_services_if_needed
+    python3 tools/autoupdate_app_sources/autoupdate_app_sources.py \
+        --edit --commit --pr --paste -j1 \
+    &> $log || sendxmpppy "[appsourcesautoupdate] App sources auto-update failed miserably"
+}
+
+function update_app_levels()
+{
+    pushd tools/update_app_levels >/dev/null
+        python3 update_app_levels.py
+    popd >/dev/null
+}
+
+function fetch_main_dashboard()
+{
+    pushd store >/dev/null
+        venv/bin/python3 fetch_main_dashboard.py
+    popd >/dev/null
+}
+
+
+function fetch_level_history()
+{
+    pushd store >/dev/null
+        venv/bin/python3 fetch_level_history.py
+    popd >/dev/null
+}
+
+$1
