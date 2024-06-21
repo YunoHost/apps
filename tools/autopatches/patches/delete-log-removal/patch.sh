62eba54291b17f34fb846756b4f3cdeb56137222
OniriCorpe <github@oniricorpe.eu>
2024-06-21 21:44:15 +0200

    Typo

--- tools/autopatches/patches/delete-log-removal/patch.sh
+++ tools/autopatches/patches/delete-log-removal/patch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sed -E "/(ynh_secure_remove|ynh_safe_rm|rm).*(\/var\/log\/)/d" --i scripts/remove
+sed -E "/(ynh_secure_remove|ynh_safe_rm|rm).*(\/var\/log\/)/d" -i scripts/remove
 
 git add scripts/remove
